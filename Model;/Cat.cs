b4fb8fa8bb40b42a7c35dd490436bb3cee0ee4e0
Bennett <Vasa0000-000@mail.ru>
2017-05-03 17:41:02 +0700

    Add project files.

---
+++ Model;/Cat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cat
+    {
+        public Cat(string age)
+        {
+            Age = age;
+            _health = 5;
+            Color = new CatColor();
+        }
+
+        private int _health;
+        private string _name;
+
+        public readonly string Age;
+        public CatColor Color { get; set; }
+        public string Name
+        {
+            get { return _name ?? "У вашей кошки нету имени"; }
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                    _name = value;
+            }
+        }
+        public string CurrentColor
+        {
+            get { return _health >= 5 ? CurrentColor = Color.HeathyColor : CurrentColor = Color.SickColor; }
+            set
+            {
+                if (_health >= 5)
+                    Color.HeathyColor = value;
+                else
+                    Color.SickColor = value;
+            }
+        }
+
+        public void Feed()
+        {
+            _health++;
+        }
+
+        public void Punish()
+        {
+            _health--;
+        }
+    }
+}
---
+++ ikit-mita-hw1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ikit_mita_hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Кошка какого возроста вам нужна?");
+            Console.Write("Решение: ");
+            string age = Console.ReadLine();
+            var cat = new Cat(age);
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Информация о кошке: \nИмя: {0}\nВозраст: {1}\nТекущий цвет: {2}"
+                    , cat.Name, cat.Age, cat.CurrentColor);
+                Console.WriteLine("\n1) Задать имя\n2) Задать цвет\n3) Ударить\n4) Покормить");
+                Console.Write("\nРешение: ");
+                int selectionMenu;
+                try
+                {
+                    selectionMenu = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadKey();
+                    continue;
+                }
+                switch (selectionMenu)
+                {
+                    case 1:
+                        Console.Write("Имя: ");
+                        cat.Name = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Новый цвет вашей кошки: ");
+                        string color = Console.ReadLine();
+                        cat.CurrentColor = color;
+                        break;
+                    case 3:
+                        cat.Punish();
+                        break;
+                    case 4:
+                        cat.Feed();
+                        break;
+                }
+            }
+        }
+    }
+}
